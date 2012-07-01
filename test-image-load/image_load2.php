a60a3d9c680ed3b3ccf898fab9712a3b7d15b459
zulfiker ali <zav_bd@yahoo.co.jp>
2012-07-02 00:36:02 +0900

    20120702 commit
    commit

---
+++ test-image-load/image_load2.php
@@ -0,0 +1,105 @@
+<html>
+<head>
+<script src="./js/jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="./js/jquery.lazyload.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+<!--
+function getImage(pExistingImageID, pImageURL){
+    var img = document.createElement('img');
+    img.onload = function (evt) {
+        document.getElementById(pExistingImageID).src=this.src;
+        document.getElementById(pExistingImageID).width=this.width;
+        document.getElementById(pExistingImageID).height=this.height;
+    }
+    img.src = pImageURL;
+    return false;
+}
+//-->
+</script>
+<title>Remote Image 5</title>
+</head>
+
+<body>
+<p><a href="#" onclick="return getImage('placeHolder','./images/image_1.jpg');">Get Remote Image</a></p>
+<p><img id="placeHolder" border="0" src="./images/image_2.jpg"></p>
+<p>
+Beijing, Jun 18 (bdnews24.com/Reuters) - Chinese astronauts carried out a manned docking with an experimental space module on Monday, the latest milestone in China's ambitious campaign to build a space station. 
+
+The Shenzhou 9 and its three-person crew, which includes China's first woman in space Liu Yang, linked with the Tiangong (Heavenly Palace) 1 module, with state television showing the pictures live. 
+
+Almost three hours later, the blue jumpsuit-wearing mission commander, Jing Haipeng, entered the module followed by colleague Liu Wang and Liu Yang, the first time China has been able to transfer astronauts between two orbiting craft. 
+
+Rendezvous and docking exercises between the two vessels are an important hurdle in China's efforts to acquire the technological and logistical skills to run a full space lab that can house astronauts for long periods. 
+
+During the 13-day mission, the astronauts will work and sleep aboard Tiangong 1, a trial module that includes an exercise bike and a video telephone booth, according to media. 
+
+The mission has been accompanied by a blaze of national pride and has been given blanket coverage by state media, down to discussion on how flying a space ship is a bit like driving a car and how the astronauts will be able to spice up their food with chilli sauce. 
+
+China is still far from catching up with the established space superpowers, the United States and Russia. The Tiangong 1 is a trial module, not the building block of a space station. 
+
+But the docking mission is the latest show of China's growing prowess in space and comes while budget restraints and shifting priorities have held back US manned space launches. 
+
+This is China's fourth manned space mission since 2003 when astronaut Yang Liwei became the country's first person in orbit. 
+
+The United States will not test a new rocket to take people into space until 2017, and Russia has said manned missions are no longer a priority. 
+
+But NASA has begun investing in US firms to provide commercial spaceflight services and is spending about $3 billion a year on a new rocket and capsule to send astronauts to the moon, asteroids and eventually to Mars. 
+
+China plans an unmanned moon landing and deployment of a moon rover. Scientists have raised the possibility of sending a man to the moon, but not before 2020. 
+
+bdnews24.com/zk/1634h 
+</p>
+<p>
+	Beijing, Jun 18 (bdnews24.com/Reuters) - Chinese astronauts carried out a manned docking with an experimental space module on Monday, the latest milestone in China's ambitious campaign to build a space station. 
+
+The Shenzhou 9 and its three-person crew, which includes China's first woman in space Liu Yang, linked with the Tiangong (Heavenly Palace) 1 module, with state television showing the pictures live. 
+
+Almost three hours later, the blue jumpsuit-wearing mission commander, Jing Haipeng, entered the module followed by colleague Liu Wang and Liu Yang, the first time China has been able to transfer astronauts between two orbiting craft. 
+
+Rendezvous and docking exercises between the two vessels are an important hurdle in China's efforts to acquire the technological and logistical skills to run a full space lab that can house astronauts for long periods. 
+
+During the 13-day mission, the astronauts will work and sleep aboard Tiangong 1, a trial module that includes an exercise bike and a video telephone booth, according to media. 
+
+The mission has been accompanied by a blaze of national pride and has been given blanket coverage by state media, down to discussion on how flying a space ship is a bit like driving a car and how the astronauts will be able to spice up their food with chilli sauce. 
+
+China is still far from catching up with the established space superpowers, the United States and Russia. The Tiangong 1 is a trial module, not the building block of a space station. 
+
+But the docking mission is the latest show of China's growing prowess in space and comes while budget restraints and shifting priorities have held back US manned space launches. 
+
+This is China's fourth manned space mission since 2003 when astronaut Yang Liwei became the country's first person in orbit. 
+
+The United States will not test a new rocket to take people into space until 2017, and Russia has said manned missions are no longer a priority. 
+
+But NASA has begun investing in US firms to provide commercial spaceflight services and is spending about $3 billion a year on a new rocket and capsule to send astronauts to the moon, asteroids and eventually to Mars. 
+
+China plans an unmanned moon landing and deployment of a moon rover. Scientists have raised the possibility of sending a man to the moon, but not before 2020. 
+
+bdnews24.com/zk/1634h 
+</p>
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_1.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_2.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_3.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_4.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_5.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_6.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_7.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_8.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_9.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_10.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_11.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_12.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_13.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_14.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_11.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_5.jpg"  width="640" heigh="480">
+<img class="lazy" border="2" src="./images/grey.gif" original="./images/image_8.jpg"  width="640" heigh="480">
+<script type="text/javascript">
+//$("img.lazy").lazyload();
+$("img.lazy").each(function() {
+    $(this).attr("src", $(this).attr("original"));
+    $(this).removeAttr("original");
+});
+
+</script>
+</body>
+</html>
